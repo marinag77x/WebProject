fa9497d0110888836ed844936b143d01c8db3803
Marina Gedjakouchian <marinag@paradox.com>
2018-05-29 12:17:41 -0400

    Add project files.

---
+++ WpfApplication1/DistributorList.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for DistributorList.xaml
+    /// </summary>
+    public partial class DistributorList : Page
+    {
+        public DistributorList()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            // ... Create a List of objects.
+
+            DBParadoxEntities db = new DBParadoxEntities();
+            var empQuery = from emp in db.DISTRIBUTORs
+                           select emp;
+
+            List<DISTRIBUTOR> items = new List<DISTRIBUTOR>();
+            items = empQuery.ToList();
+   
+           // ... Assign ItemsSource of DataGrid.
+            var grid = sender as DataGrid;
+            grid.ItemsSource = items;
+        }
+       
+
+    }
+}
---
+++ WpfApplication1/WebPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for WebPage.xaml
+    /// </summary>
+    public partial class WebPage : Page
+    {
+        public WebPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            DistributorList DistributorListPage = new DistributorList();
+            this.NavigationService.Navigate(DistributorListPage);
+
+        }
+
+    }
+}
